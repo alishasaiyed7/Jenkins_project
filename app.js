b971f10220deec707ab76f2a78411e680d5ca2a4
ALISHA SAIYED <106082357+alishasaiyed7@users.noreply.github.com>
2025-04-05 18:02:16 +0530

    Update app.js

--- app.js
+++ app.js
@@ -97,7 +97,6 @@ app.use('/stories', require('./routes/stories'))
 
 const PORT = process.env.PORT || 3000
 
-app.listen(
-  PORT,
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
-)
+})
